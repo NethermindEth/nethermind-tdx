1764bf6d43c79b4676260ebe6d335cfc8a8d4f2c
Franco Barpp Gomes <franco@nethermind.io>
2024-09-02 16:36:50 -0300

    feat: Add yocto setup script

---
+++ scripts/setup_yocto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm python3-subunit mesa-common-dev zstd liblz4-tool chrpath diffstat lz4 mtools
+sudo locale-gen en_US.UTF-8
+
+sudo apt install repo
+
+mkdir -p build
