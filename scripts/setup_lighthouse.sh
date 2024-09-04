9ca5fa71b62f730668b6b0b6a7ccd59c3581190d
Franco Barpp Gomes <franco@nethermind.io>
2024-09-04 14:49:33 -0300

    feat: Only clone lighthouse if directory does not exist

--- scripts/setup_lighthouse.sh
+++ scripts/setup_lighthouse.sh
@@ -4,8 +4,12 @@
 
 set -e
 
+if [ ! -d "lighthouse" ]; then
 git clone https://github.com/sigp/lighthouse
+fi
+
 cd lighthouse
+
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --target x86_64-unknown-linux-gnu -p lighthouse --release
 
 cd ..
