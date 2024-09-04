da42ccea9a01976df598f149e5f44187863b9b45
Franco Barpp Gomes <franco@nethermind.io>
2024-09-04 15:16:41 -0300

    feat: Only generate patches if not up to date

--- patches/generate_patches.sh
+++ patches/generate_patches.sh
@@ -10,8 +10,23 @@ for phase in post pre; do
                 new_file="$dir/$base_name.new"
                 patch_file="$dir/$base_name.patch"
 
-                echo "Generating patch for $base_name in $phase"
+                echo "Processing $base_name in $phase"
 
+                if [ -f "$patch_file" ]; then
+                    should_skip=false
+
+                    temp_file=$(mktemp)
+                    patch -s -f "$file" "$patch_file" -o "$temp_file"
+                    cmp -s "$temp_file" "$new_file" && should_skip=true
+                    rm "$temp_file"
+
+                    if [ "$should_skip" = true ]; then
+                        echo "Patch for $base_name is up to date"
+                        continue
+                    fi
+                fi
+
+                echo "Generating patch for $base_name"
                 diff -u "$file" "$new_file" > "$patch_file" || true
             done
         fi
