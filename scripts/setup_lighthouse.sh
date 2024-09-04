7a82c0b34bc04e6b6dd97b9f3624dac15acd032d
Franco Barpp Gomes <franco@nethermind.io>
2024-09-04 13:03:35 -0300

    feat: Add bin copy to setup_lighthouse

--- scripts/setup_lighthouse.sh
+++ scripts/setup_lighthouse.sh
@@ -7,3 +7,6 @@ set -e
 git clone https://github.com/sigp/lighthouse
 cd lighthouse
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --target x86_64-unknown-linux-gnu -p lighthouse --release
+
+cd ..
+cp lighthouse/target/x86_64-unknown-linux-gnu/release/lighthouse meta-lighthouse-bin/recipes-nodes/lighthouse/lighthouse
