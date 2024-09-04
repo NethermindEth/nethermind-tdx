c2f3ed3df251e282d4944acd9741bc739e0b5065
Franco Barpp Gomes <franco@nethermind.io>
2024-09-04 14:50:07 -0300

    refactor: Remove unnecessary cd in setup_lighthouse

--- scripts/setup_lighthouse.sh
+++ scripts/setup_lighthouse.sh
@@ -5,12 +5,11 @@
 set -e
 
 if [ ! -d "lighthouse" ]; then
-git clone https://github.com/sigp/lighthouse
+    git clone https://github.com/sigp/lighthouse
 fi
 
 cd lighthouse
 
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --target x86_64-unknown-linux-gnu -p lighthouse --release
 
-cd ..
-cp lighthouse/target/x86_64-unknown-linux-gnu/release/lighthouse meta-lighthouse-bin/recipes-nodes/lighthouse/lighthouse
+cp target/x86_64-unknown-linux-gnu/release/lighthouse ../meta-lighthouse-bin/recipes-nodes/lighthouse/lighthouse
