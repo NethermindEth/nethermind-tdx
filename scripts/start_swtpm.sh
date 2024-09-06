657e372a3c91154564858a8eaaf73af29a4ddb76
Franco Barpp Gomes <franco@nethermind.io>
2024-09-06 18:02:07 -0300

    fix: Set sudo only in necessary commands

--- scripts/start_swtpm.sh
+++ scripts/start_swtpm.sh
@@ -7,12 +7,14 @@ set -e
 sudo apt install swtpm swtpm-tools -y
 
 mkdir /tmp/tdxqemu-tpm || true
-swtpm_setup --tpmstate /tmp/tdxqemu-tpm \
+
+sudo swtpm_setup --tpmstate /tmp/tdxqemu-tpm \
   --create-ek-cert \
   --create-platform-cert \
   --create-spk \
   --tpm2 \
   --overwrite
+
 swtpm socket --tpmstate dir=/tmp/tdxqemu-tpm \
   --ctrl type=unixio,path=/tmp/tdxqemu-tpm/swtpm-sock \
   --tpm2 \
