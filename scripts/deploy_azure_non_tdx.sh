6c2608a811e925bf44290900a0ed1aba37b1d0b7
Franco Barpp Gomes <franco@nethermind.io>
2024-09-25 11:16:09 -0300

    fix: Revert non-tdx size to Standard_E4s_v5

--- scripts/deploy_azure_non_tdx.sh
+++ scripts/deploy_azure_non_tdx.sh
@@ -21,7 +21,7 @@ for var in ALLOWED_IP; do
 done
 
 VM_NAME="non-tdx"
-AZURE_VM_SIZE="Standard_EC4es_v5"
+AZURE_VM_SIZE="Standard_E4s_v5"
 AZURE_STORAGE_GB=600
 OS_DISK_SKU="Premium_LRS"
 STORAGE_DISK_SKU="Premium_LRS"
