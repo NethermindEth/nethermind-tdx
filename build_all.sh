b930cf540a74e660f9713c9ff098c9ba82488dc2
Franco Barpp Gomes <franco@nethermind.io>
2024-10-06 00:44:01 +0100

    feat: Add build_all and deploy_all utilities

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+rm -rf artifacts
+
+for node in geth nethermind reth; do
+    echo "Building $node"
+
+    rm -rf "${node}-artifacts"
+
+    sed -i "s/nethermind|reth|geth/${node}/g" patches/post/cvm-initramfs.bb.new
+
+    mv artifacts "${node}-artifacts"
+done
---
+++ deploy_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$ALLOWED_IP" ]; then
+    echo "ALLOWED_IP is not set"
+    exit 1
+fi
+
+for node in geth nethermind reth; do
+    echo "Deploying $node"
+
+    make deploy-azure DISK_PATH="${node}-artifacts/dev/cvm-image-azure-tdx.rootfs.wic.vhd" ALLOWED_IP=${ALLOWED_IP} VM_NAME=test-${node} CONFIG_PATH=./config.json
+done
