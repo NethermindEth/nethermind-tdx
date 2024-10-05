3cee4c9b793454e00f63044991064134168e3045
Franco Barpp Gomes <franco@nethermind.io>
2024-10-06 00:59:09 +0100

    feat: Ignore artifacts rm error in build_all

--- build_all.sh
+++ build_all.sh
@@ -7,7 +7,7 @@ rm -rf artifacts
 for node in geth nethermind reth; do
     echo "Building $node"
 
-    rm -rf "${node}-artifacts"
+    rm -rf "${node}-artifacts" || true
 
     sed -i "s/nethermind|reth|geth/${node}/g" patches/post/cvm-initramfs.bb/cvm-initramfs.bb.new
 
