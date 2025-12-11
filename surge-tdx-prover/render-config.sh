a8764497f7c62955f3ca826bd5c8940dc6198d36
Franco Barpp Gomes <franco@nethermind.io>
2025-12-11 09:07:53 -0300

    feat: Comment out raiko patching

--- surge-tdx-prover/render-config.sh
+++ surge-tdx-prover/render-config.sh
@@ -19,9 +19,9 @@ find surge-tdx-prover/mkosi.extra -type f -name "*.mustache" | while read -r tem
 done
 
 # TODO: remove this once not necessary anymore
-L1_CONTRACT=$(jq -r '.raiko.l1_contract' "$ENV_FILE")
-L2_CONTRACT=$(jq -r '.raiko.l2_contract' "$ENV_FILE")
-grep -q "0xa3c616dd54F6BB35a736cD6968c8EF7176faCACc" "$BUILDROOT/usr/bin/raiko" || { echo "Error: Expected default L1 contract address not found"; exit 1; }
-grep -q "0x7633740000000000000000000000000000010001" "$BUILDROOT/usr/bin/raiko" || { echo "Error: Expected default L2 contract address not found"; exit 1; }
-sed -i "s/0xa3c616dd54F6BB35a736cD6968c8EF7176faCACc/$L1_CONTRACT/g" "$BUILDROOT/usr/bin/raiko"
-sed -i "s/0x7633740000000000000000000000000000010001/$L2_CONTRACT/g" "$BUILDROOT/usr/bin/raiko"
+# L1_CONTRACT=$(jq -r '.raiko.l1_contract' "$ENV_FILE")
+# L2_CONTRACT=$(jq -r '.raiko.l2_contract' "$ENV_FILE")
+# grep -q "0xa3c616dd54F6BB35a736cD6968c8EF7176faCACc" "$BUILDROOT/usr/bin/raiko" || { echo "Error: Expected default L1 contract address not found"; exit 1; }
+# grep -q "0x7633740000000000000000000000000000010001" "$BUILDROOT/usr/bin/raiko" || { echo "Error: Expected default L2 contract address not found"; exit 1; }
+# sed -i "s/0xa3c616dd54F6BB35a736cD6968c8EF7176faCACc/$L1_CONTRACT/g" "$BUILDROOT/usr/bin/raiko"
+# sed -i "s/0x7633740000000000000000000000000000010001/$L2_CONTRACT/g" "$BUILDROOT/usr/bin/raiko"
