5275203b6224aab0ab28d36fb2ce9461f59f1ee1
Franco Barpp Gomes <franco@nethermind.io>
2025-12-11 00:57:42 -0300

    fix: Fix TDXS version

--- services/tdxs/build.sh
+++ services/tdxs/build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 source scripts/make_git_package.sh
 
-TDXS_VERSION="main"
+TDXS_VERSION="master"
 TDXS_GIT_URL="https://github.com/Hyodar/tdxs"
 TDXS_BINARY_PATH="/usr/bin/tdxs"
 
